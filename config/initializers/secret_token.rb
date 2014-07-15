ae92a878507aaab9f2d647a5bb6d5fb200715950
user <user@Sloboda.(none)>
2014-07-15 22:41:10 +0300

    First lesson

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bykova::Application.config.secret_key_base = 'e1d803d8e597967ad0be58462c43b2692e41abb4fa8b0cd3c6f507560e8ee2a639a8fdcdaeaa8305acdc4e1d0fc0a8338f8b9ba0c76944dfbe9e774c06a2e87c'
